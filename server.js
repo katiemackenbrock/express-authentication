1b94ea067fd41c18c20e4eb1a5240aebf379e766
katiemackenbrock <katiemackenbrock@gmail.com>
2021-01-21 18:17:52 -0800

    no errors done

--- server.js
+++ server.js
@@ -1,20 +1,47 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
+const session = require('express-session');
+const flash = require('connect-flash');
+const helmet = require('helmet');
+const passport = require('./config/ppConfig');
+const isLoggedIn = require('./middleware/isLoggedIn');
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
+// allows us to read form sent body data
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+app.use(helmet());
+
+app.use(session({
+  secret: process.env.SESSION_SECRET, // should be an ENV variable
+  resave: false,
+  saveUninitialized: true
+}));
+
+// Init passport config MUST HAPPEN AFTER SESSION CONFIG
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// Write custom middleware to access the user on every response
+app.use((req, res, next) => {
+  let alerts = req.flash();
+  console.log(alerts);
+  res.locals.alerts = alerts;
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/profile', (req, res) => {
+app.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile');
 });
 
